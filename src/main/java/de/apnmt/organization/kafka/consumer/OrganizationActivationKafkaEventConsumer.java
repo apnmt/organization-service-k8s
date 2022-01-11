b5e9619190c2d09f24787df4bb0c36f4bcf722d9
TWI <audi-connect@msg.group>
2022-01-11 11:19:48 +0100

    remove copyright [skip ci]

--- src/main/java/de/apnmt/organization/kafka/consumer/OrganizationActivationKafkaEventConsumer.java
+++ src/main/java/de/apnmt/organization/kafka/consumer/OrganizationActivationKafkaEventConsumer.java
@@ -1,13 +1,3 @@
-/*
- * OrganizationActivationEventConsumer.java
- *
- * (c) Copyright AUDI AG, 2021
- * All Rights reserved.
- *
- * AUDI AG
- * 85057 Ingolstadt
- * Germany
- */
 package de.apnmt.organization.kafka.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
